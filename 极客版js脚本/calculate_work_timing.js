4e49e356d80b330148234d5a5e50286f03a7edff
Derstood <1831003848@qq.com>
2025-03-12 11:45:19 +0800

    Update calculate_work_timing.js

--- 极客版js脚本/calculate_work_timing.js
+++ 极客版js脚本/calculate_work_timing.js
@@ -18,6 +18,7 @@
 
     // 用于累计超过 11 小时的时间差
     let totalExcessTime = 0;
+    let totalTime = 0;
     // 将毫秒转化为半小时为单位的时间
     function roundToHalfHour(ms) {
       // 计算超出的小时数（单位：小时）
@@ -40,8 +41,12 @@
           const startTime = new Date(`1970-01-01T${startTimeStr}Z`);
           const endTime = new Date(`1970-01-01T${endTimeStr}Z`);
 
+          if (isNaN(startTime.getTime()) || isNaN(endTime.getTime())) {
+            return
+          }
           // 计算时间差（毫秒）
           const timeDifference = endTime - startTime;
+          totalTime += timeDifference / 3600000;
           // 如果差值超过 11 小时 (11小时 = 11 * 60 * 60 * 1000 毫秒)
           if (timeDifference >= 11.5 * 60 * 60 * 1000) {
             // 累加超出部分，四舍五入到最接近的半小时
@@ -53,6 +58,8 @@
 
     });
     console.log('超过 11 小时的累计时间差（按半小时计算）：', totalExcessTime, '小时');
+    console.log('总工时 不包含周末：', totalTime.toFixed(2), '小时');
+    console.log(totalTime);
   }
 
   // 选择需要绑定事件的按钮
@@ -66,4 +73,6 @@
   if (nextButton) {
       nextButton.addEventListener('click', calculate_time);
   }
+
+
 })();
